d2fb96bd240478e7f182b13447cf40b1c8117360
ktronix97 <daliaestefany.1701421354@ucaldas.edu.co>
2020-03-14 16:35:33 -0500

    Modificacion de registro 3.0v

--- src/app/modules/cliente/cliente-routing.module.ts
+++ src/app/modules/cliente/cliente-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegistroComponent } from './registro/registro.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  
+{
+  path:'registro',
+  component:RegistroComponent
+}
+,
+{
+  path: '',
+  pathMatch:'full',
+  redirectTo:'/home'
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/cliente/registro/registro.component.ts
+++ src/app/modules/cliente/registro/registro.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormBuilder,Validators} from '@angular/forms';
+import{Router}from'@angular/router';
+import{SecurityService}from'src/app/services/security.service';
+
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +11,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  fgValidation:FormGroup;
+
+  constructor(private fb:FormBuilder, private secService:SecurityService,
+    
+    private router:Router) { }
+
 
   ngOnInit() {
+    this.fgValidationBuilder();
+    this.loginEvent();
+  }
+
+  fgValidationBuilder(){
+    this.fgValidation=this.fb.group({
+      username:['',[Validators.required,Validators.maxLength(30),Validators.minLength(8),Validators.email]],
+      password:['',[Validators.required,Validators.minLength(5),Validators.maxLength(15)]]
+    });
+  }
+
+  get fg(){
+    return this.fgValidation.controls;
+
+  }
+
+  loginEvent(){
+    if(this.fgValidation.invalid){
+      
+    }
+    else{
+      let u =this.fg.username.value;
+      let p=this.fg.password.value;
+      let user=this.secService.loginUser(u,p);
+      if (user != null){
+          this.router.navigate(['/home']);
+      }
+    }
   }
 
 }
+
